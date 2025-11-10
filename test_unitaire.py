028d2ff4daf62d68fbf4b071116160273683340d
Zackginger <2551478@cegepoutaouais.qc.ca>
2025-11-10 13:57:17 -0500

    plan test et test unitaire Zackary

--- test_unitaire.py
+++ test_unitaire.py
@@ -3,7 +3,12 @@ from module import *
 
 
 @pytest.mark.parametrize("code , essai , dictindice", [
-                         (["rose","vert","bleu","rouge"],["vert","rose","gris","orange"],{"white":0,"black":2,"null":2})
+                        (["rose","vert","bleu","rouge"],["vert","rose","gris","orange"],{"white":0,"black":2,"null":2}),
+                        (["vert","vert","vert","vert"],["vert","vert","vert","vert"],{"white":4,"black":0,"null":0}),
+                        (["rose","rose","rose","rose"],["rouge","bleu","jaune","rouge"],{"white":0,"black":0,"null":4}),
+                        (["bleu","vert","bleu","vert"],["vert","bleu","vert","bleu"],{"white":0,"black":4,"null":0}),
+                        (["bleu","bleu","rouge","orange"],["bleu","vert","orange","jaune"],{"white":1,"black":1,"null":2})
+
 ])
 def testpoint(code,essai,dictindice):
 
